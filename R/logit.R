20645ab9301b011d8cd4419051167de2a1f3b175
Taragin <Charles.Taragin@usdoj.gov>
2017-12-26 00:07:42 -0500

    fixed bug!

--- R/logit.R
+++ R/logit.R
@@ -314,8 +314,8 @@ setMethod(
       
               outVal <- ifelse(object@shareInside<1, 1, 0)
               
-              VPre  <- sum(exp(meanval + object@pricePre*alpha) + outVal)
-              VPost <- sum(exp(meanval + object@pricePost*alpha)[subset] + outVal)
+              VPre  <- sum(exp(meanval + object@pricePre*alpha))  + outVal
+              VPost <- sum(exp(meanval + object@pricePost*alpha)[subset] ) + outVal
 
               result <- log(VPost/VPre)/alpha
 
