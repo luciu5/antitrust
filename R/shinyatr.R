43c9c8874e1a49e37fe6a2b5debce1fcefdf4bee
Taragin <Charles.Taragin@usdoj.gov>
2018-02-13 17:27:19 -0500

    fixed bug preventing Cournot from working

--- R/shinyatr.R
+++ R/shinyatr.R
@@ -24,12 +24,12 @@ require(rhandsontable)
                                 )),
         conditionalPanel(
           condition = "input.supply == 'Bertrand'",
-          selectInput("demand", "Demand Specification:",
+          selectInput("demand_bert", "Demand Specification:",
                       choices = c("logit", "ces", "linear","aids"))
         ),
         conditionalPanel(
           condition = "input.supply == 'Cournot'",
-          selectInput("demand", "Demand Specification:",
+          selectInput("demand_cournot", "Demand Specification:",
                       choices = c("linear","log"))
         ),
         conditionalPanel(
@@ -43,7 +43,7 @@ require(rhandsontable)
         # ),
         conditionalPanel(
           condition = "input.supply == '2nd Score Auction'",
-          selectInput("demand", "Demand Specification:",
+          selectInput("demand_2nd", "Demand Specification:",
                       choices = c("Logit"))
         )
       ),
@@ -57,10 +57,7 @@ require(rhandsontable)
 
       )
       
-    ),
-    hr(),
-    print("Created by Charles Taragin (2018).The views expressed herein are entirely those of the authors and should not be purported to reflect those of the U.S. Department of Justice.  This merger simulator is released into the
-          public domain without warranty of any kind, expressed or implied.")
+    )
   )
   )
 
@@ -68,16 +65,15 @@ require(rhandsontable)
 
 
    nPossProds <- 10 #only allow 10 products
-
-   genInputData <- function(n
-                            #,s
-                            ){
+   
+   
+   genInputData <- function(){
 
      
      inputData <- data.frame(
-       Name = c("BUD","OLD STYLE","MILLER"),
-       ownerPre  = c("BUD","OLD STYLE","MILLER"),
-       ownerPost = c("BUD","BUD","MILLER"),
+       Name = c("P1","P2","P3"),
+       ownerPre  = c("O1","O2","O3"),
+       ownerPost = c("O1","O1","O3"),
        Prices     = c(.0441,.0328,.0409),
        Shares    = c( 0.1344196, 0.3503055, 0.5152749),
        Margins   = c(.3830,.5515,.5421)
@@ -89,22 +85,7 @@ require(rhandsontable)
      inputData[(nDefProd+1):nPossProds,] <- NA
      rownames(inputData) <- NULL
      
-     # inputData <- data.frame(Firm=1:input$nProds,
-     #                         Prices=NA_real_,
-     #                         Shares=NA_real_,
-     #                         Margins=NA_real_,
-     #                         ownerPre=factor(1:n),
-     #                         ownerPost=factor(1:n),
-     #                         mcDelta=0)
-
-     #inputData$ownerPost[inputData$ownerPost==2]=1
-     
-     
-     #if(s =="Cournot"){
-     #   colnames(inputData)[colnames(inputData) == "Shares"] = "Quantities"
-     # inputData$Prices <- NULL
-     #}
-
+    
      return(inputData)
 
    }
@@ -115,22 +96,20 @@ require(rhandsontable)
 
     observe({
 
-      nProds <- input$nProds
-      #supply <- input$supply
       
-     values$inputData <- genInputData(nProds#, input$supply
-                                      )
+     values$inputData <- genInputData()
 
     })
 
     observe({
 
-
+      input$supply
 
       if (!is.null(input$hot) && input$simulate == 0) {
         values$inputData = hot_to_r(input$hot)
       }
     })
+    
 
     output$hot <- renderRHandsontable({
       inputData <- values[["inputData"]]
@@ -142,10 +121,12 @@ require(rhandsontable)
     
     sims <- reactive({
       
+      
       supply <- input$supply
-      demand <- input$demand
-      cost <-   input$cost
-      mktPrice <- input$mktPrice
+      demand <- ifelse(supply =="Bertrand", input$demand_bert,
+                       ifelse(supply == "Cournot", input$demand_cournot, input$demand_2nd))
+      cost <- input$cost 
+     
       indata <- values[["inputData"]]
       indata <- indata[!is.na(indata$Shares),]
       if(!input$incEff) indata$mcDelta <- 0
@@ -163,7 +144,7 @@ require(rhandsontable)
         switch(supply,
                Bertrand =
                  switch(demand,
-                        logit = logit(prices= indata$Prices,
+                        logit= logit(prices= indata$Prices,
                                       shares= indata$Shares,
                                       margins= indata$Margins,
                                       ownerPre= ownerPre,
@@ -188,28 +169,14 @@ require(rhandsontable)
                                   ownerPost= ownerPost,
                                   mcDelta = indata$mcDelta)
                  ),
-               Cournot =
-                 switch(demand,
-                        linear = 
-                          cournot(prices = .5, quantities = matrix(c(100,10,50)), margins = matrix(c(.5,NA,NA)),ownerPre  =diag(3),ownerPost=matrix(c(1,1,0,1,1,0,0,0,1),ncol=3),mcDelta=rep(0,3))
-                          
-                          # cournot(prices= mktPrice,
-                          #                demand = "linear",
-                          #                cost= rep(cost, nrow(indata)),
-                          #                quantities = as.matrix(indata$Shares),
-                          #                margins= as.matrix(indata$Margins),
-                          #                ownerPre= ownerPre,
-                          #                ownerPost= ownerPost,
-                          #                mcDelta = indata$mcDelta),
-                        # log = cournot(prices= input$mktPrice,
-                        #                        demand =  "log",
-                        #                        cost= rep(cost, nrow(indata)),
-                        #                      quantities = as.matrix(indata$shares),
-                        #                      margins= as.matrix(indata$Margins),
-                        #                      ownerPre= ownerPre,
-                        #                      ownerPost= ownerPost,
-                        #                      mcDelta = indata$mcDelta)
-                        ),
+               Cournot =                           cournot(prices= indata$Prices[1],
+                                                           demand = demand,
+                                                           cost= rep(cost, nrow(indata)),
+                                                           quantities = as.matrix(indata$Shares),
+                                                           margins= as.matrix(indata$Margins),
+                                                           ownerPre= ownerPre,
+                                                           ownerPost= ownerPost,
+                                                           mcDelta = indata$mcDelta),
                `2nd Score Auction`=auction2nd.logit(prices= indata$Prices,
                                                     shares= indata$Shares,
                                                     margins= indata$Margins,
@@ -230,14 +197,13 @@ require(rhandsontable)
     output$results <-
           
           renderPrint({
-            
-            
+           
             #nQuant <- length(!is.na(values[["inputData"]]$Shares))
             #nQuant <- ifelse(length(nQuant)>0, nQuant,0)
             
+            thisSim <- sims()
             if(input$simulate == 0) return(cat("Enter Inputs"))
-            summary(sims(),market= !inputdispDetails
-                    )
+            summary(thisSim,market= !input$dispDetails)
           })
         
       
