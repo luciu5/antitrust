2eaced18a138fdf2cf67756de564a85fbe27f7e9
Taragin <Charles.Taragin@usdoj.gov>
2018-02-27 16:14:36 -0500

    made the following changes suggested by Luke: changed product and firm labels, added table of product elasticities, added the ability to include an elasticity.

--- inst/antitrust_shiny/server.R
+++ inst/antitrust_shiny/server.R
@@ -12,9 +12,9 @@ shinyServer(function(input, output, session) {
 
      
      inputData <- data.frame(
-       Name = c("P1","P2","P3","P4"),
-       ownerPre  = c("O1","O2","O3","O3"),
-       ownerPost = c("O1","O1","O3","O3"),
+       Name = c("Prod1","Prod2","Prod3","Prod4"),
+       ownerPre  = c("Firm1","Firm2","Firm3","Firm3"),
+       ownerPost = c("Firm1","Firm1","Firm3","Firm3"),
        #'Prices (\u00A4)'  = c(6.46, 6.25, 6.10),
        #Shares = c( 0.2643934, 0.3438824, 0.3917243),
        #Margins = c(0.3, 0.4,0.28),
@@ -65,12 +65,12 @@ shinyServer(function(input, output, session) {
            'Producer Benefit (\u00A4/unit)' = thispsdelta,
            'Overall Effect (\u00A4/unit)'= -1*thiscv + thispsdelta,
            'Compensating Marginal Cost Reduction (\u00A4/unit)' = ifelse(supply == "Cournot", thiscmcr, sum(thiscmcr*s$sharesPost[s$isParty== "*"]/sum(s$sharesPost[s$isParty== "*"],na.rm=TRUE))),
-           'Market Elasticity' = thiselast,
+           'Estimated Market Elasticity' = thiselast,
            check.names=FALSE
      )
      
-     if(demand %in% c("aids","ces")){
-       res$'Overall Effect ($/unit)' <- NULL
+     if(demand %in% c("aids","ces","pcaids")){
+       res$'Overall Effect (\u00A4/unit)' <- NULL
        colnames(res) <- gsub('(?<=Consumer Harm\\s)\\(\\\u00A4/unit\\)',"(% Expenditure)",colnames(res), perl=TRUE)
        }
      
@@ -94,9 +94,16 @@ shinyServer(function(input, output, session) {
     observe({
 
        supply <- input$supply
-      demand <- ifelse(supply =="Bertrand", input$demand_bert,
-                       ifelse(supply == "Cournot", input$demand_cournot, input$demand_2nd))
-
+       calcElast <- input$calcElast
+       mktElast <- input$enterElast
+       demand <- ifelse(supply =="Bertrand", 
+                        ifelse(calcElast,input$demand_bert_alm,input$demand_bert),
+                        ifelse(supply == "Cournot", 
+                               ifelse(calcElast,input$demand_cournot_alm, input$demand_cournot), 
+                               ifelse(calcElast,input$demand_2nd_alm, input$demand_2nd)))
+       
+       
+      
       updateCheckboxInput(session, inputId = "dispDetails", value = FALSE)      
 
       if (!is.null(input$hot) 
@@ -126,10 +133,17 @@ shinyServer(function(input, output, session) {
       if(is.null(input$hot)){return()}
       
       supply <- input$supply
-      demand <- ifelse(supply =="Bertrand", input$demand_bert,
-                       ifelse(supply == "Cournot", input$demand_cournot, input$demand_2nd))
-       
-     
+      calcElast <- input$calcElast
+      demand <- ifelse(supply =="Bertrand", 
+                       ifelse(calcElast,input$demand_bert_alm,input$demand_bert),
+                       ifelse(supply == "Cournot", 
+                              ifelse(calcElast,input$demand_cournot_alm, input$demand_cournot), 
+                              ifelse(calcElast,input$demand_2nd_alm, input$demand_2nd)))
+      
+      mktElast <- input$enterElast
+      
+      
+      
       #if(input$dispDetails){updateCheckboxInput(session, inputId = "dispDetails", value = TRUE)}
       
       indata <- values[["inputData"]]
@@ -137,6 +151,10 @@ shinyServer(function(input, output, session) {
       outstring <- colnames(indata[colnames(indata) %in% c("Quantities","Shares")])
       
       indata <- indata[!is.na(indata[,outstring]),]
+      
+      
+      firstMargin <- which(!is.na(indata$Margins))[1]
+      firstPrice <- which(!is.na(indata[,"Prices (\u00A4)"]))[1]
       
       #if(!input$incEff) indata$mcDelta <- 0
       indata$mcDelta <- 0
@@ -149,58 +167,105 @@ shinyServer(function(input, output, session) {
       ownerPre = tcrossprod(ownerPre)
       ownerPost = model.matrix(~-1+indata$ownerPost)
       ownerPost = tcrossprod(ownerPost)
-      
+    
     isolate({  
      
         switch(supply,
                Bertrand =
                  switch(demand,
-                        logit= logit.alm(prices= indata[,"Prices (\u00A4)"],
+                        `logit (unknown elasticity)`= logit.alm(prices= indata[,"Prices (\u00A4)"],
                                       shares= indata[,outstring],
                                       margins= indata$Margins,
                                       ownerPre= ownerPre,
                                       ownerPost= ownerPost,
                                       mcDelta = indata$mcDelta, labels=indata$Name),
-                        ces= ces.alm(prices= indata[,"Prices (\u00A4)"],
+                        `ces (unknown elasticity)`= ces.alm(prices= indata[,"Prices (\u00A4)"],
                                  shares= indata[,outstring],
                                  margins= indata$Margins,
                                  ownerPre= ownerPre,
                                  ownerPost= ownerPost,
                                  mcDelta = indata$mcDelta, labels=indata$Name),
-                        # linear=linear(prices= indata[,"Prices (\u00A4)"],
-                        #               quantities= indata[,outstring],
-                        #               margins= indata$Margins,
-                        #               ownerPre= ownerPre,
-                        #               ownerPost= ownerPost,
-                        #               mcDelta = indata$mcDelta, labels=indata$Name),
+                         linear=linear(prices= indata[,"Prices (\u00A4)"],
+                                       quantities= indata[,outstring],
+                                       margins= indata$Margins,
+                                       ownerPre= ownerPre,
+                                       ownerPost= ownerPost,
+                                       mcDelta = indata$mcDelta, labels=indata$Name),
                         aids=aids(prices= indata[,"Prices (\u00A4)"],
                                   shares= indata[,outstring],
                                   margins= indata$Margins,
                                   ownerPre= ownerPre,
                                   ownerPost= ownerPost,
-                                  mcDelta = indata$mcDelta, labels=indata$Name)
+                                  mcDelta = indata$mcDelta, labels=indata$Name),
+                        logit= logit.alm(prices= indata[,"Prices (\u00A4)"],
+                                     shares= indata[,outstring],
+                                     margins= indata$Margins,
+                                     ownerPre= ownerPre,
+                                     ownerPost= ownerPost,
+                                     mcDelta = indata$mcDelta, labels=indata$Name,  mktElast = mktElast ),
+                        ces = ces.alm(prices= indata[,"Prices (\u00A4)"],
+                                 shares= indata[,outstring],
+                                 margins= indata$Margins,
+                                 ownerPre= ownerPre,
+                                 ownerPost= ownerPost,
+                                 mcDelta = indata$mcDelta, labels=indata$Name,  mktElast = mktElast),
+                        linear=linear(prices= indata[,"Prices (\u00A4)"],
+                                      quantities= indata[,outstring],
+                                      margins= indata$Margins,
+                                      ownerPre= ownerPre,
+                                      ownerPost= ownerPost,
+                                      mcDelta = indata$mcDelta, labels=indata$Name),
+                        pcaids=pcaids(prices= indata[,"Prices (\u00A4)"],
+                                      shares= indata[,outstring],
+                                      knownElast = -1/indata$Margins[firstMargin],
+                                      knownElastIndex = firstMargin,
+                                      mktElast = mktElast,
+                                      ownerPre= ownerPre,
+                                      ownerPost= ownerPost,
+                                      mcDelta = indata$mcDelta, labels=indata$Name)
                  ),
-               Cournot =                           cournot(prices= indata[,"Prices (\u00A4)"][1],
-                                                           demand = demand,
-                                                           cost= rep("linear", nrow(indata)),
-                                                           quantities = as.matrix(indata[,outstring]),
-                                                           margins= as.matrix(indata$Margins),
-                                                           ownerPre= ownerPre,
-                                                           ownerPost= ownerPost,
-                                                           mcDelta = indata$mcDelta, 
-                                                           labels=list(as.character(indata$ownerPre),indata$Name[1])),
-               `2nd Score Auction`=auction2nd.logit.alm(prices= indata[,"Prices (\u00A4)"],
+               Cournot = 
+
+                                 cournot(prices= indata[,"Prices (\u00A4)"][firstPrice],
+                                                                                   demand = gsub("\\s+\\(.*","",demand,perl=TRUE),
+                                                                                   cost= rep("linear", nrow(indata)),
+                                                                                   quantities = as.matrix(indata[,outstring]),
+                                                                                   margins= as.matrix(indata$Margins),
+                                                                                   ownerPre= ownerPre,
+                                                                                   ownerPost= ownerPost,
+                                                                                   mktElast = ifelse( grepl("unknown elasticity", demand),
+                                                                                                      NA_real_, mktElast),
+                                                                                   mcDelta = indata$mcDelta, 
+                                                                                   labels=list(as.character(indata$ownerPre),indata$Name[firstPrice]))
+                                ,
+               `2nd Score Auction`= switch(demand,
+                                           `logit (unknown elasticity)` = auction2nd.logit.alm(prices= indata[,"Prices (\u00A4)"],
                                                     shares= indata[,outstring],
                                                     margins= indata$Margins,
                                                     ownerPre= ownerPre,
                                                     ownerPost= ownerPost,
-                                                    mcDelta = indata$mcDelta, labels=indata$Name)
+                                                    mcDelta = indata$mcDelta, labels=indata$Name),
+                                            logit = auction2nd.logit.alm(prices= indata[,"Prices (\u00A4)"],
+                                                                                shares= indata[,outstring],
+                                                                                margins= indata$Margins,
+                                                                                ownerPre= ownerPre,
+                                                                                ownerPost= ownerPost,
+                                                                                mcDelta = indata$mcDelta, labels=indata$Name, 
+                                                                               mktElast = mktElast)
+                                           
+               )
                
                
         )
       
       
     })
+        
+      
+      
+     
+      
+      
     })
     
     
@@ -214,8 +279,14 @@ shinyServer(function(input, output, session) {
             
             thisSim <<- sims()
             supply <- input$supply
-            demand <- ifelse(supply =="Bertrand", input$demand_bert,
-                             ifelse(supply == "Cournot", input$demand_cournot, input$demand_2nd))
+            calcElast <- input$calcElast
+            demand <- ifelse(supply =="Bertrand", 
+                             ifelse(calcElast,input$demand_bert_alm,input$demand_bert),
+                             ifelse(supply == "Cournot", 
+                                    ifelse(calcElast,input$demand_cournot_alm, input$demand_cournot), 
+                                    ifelse(calcElast,input$demand_2nd_alm, input$demand_2nd)))
+            
+            
             
             #if(input$simulate == 0) return(cat("Enter Inputs"))
             gensum(thisSim,supply,demand)
@@ -251,7 +322,20 @@ shinyServer(function(input, output, session) {
       })  
     
     
+   
     
+    output$results_elast <- renderTable({
+      
+      if(input$dispDetails ){
+        
+       if(input$pre_elast == "Pre-Merger"){ preMerger = TRUE}
+        else{preMerger =FALSE}
+        
+        res <- elast(thisSim, preMerger=preMerger)
+        print(res)
+        
+      }
+    }, rownames = TRUE)   
   
 
 
--- inst/antitrust_shiny/ui.R
+++ inst/antitrust_shiny/ui.R
@@ -6,11 +6,17 @@ shinyUI(fluidPage(
     titlePanel("Simulate a Merger"),
     sidebarLayout(
       sidebarPanel(
-        helpText("Simulate a horizontal merger between the owners of products 1 and 2.",
+        helpText("Simulate a merger between 'Firm1' and 'Firm2'\n.",
                  "Enter (or copy and paste) shares, margins, and prices in Inputs table (right).",
                  "shares must be between 0 and 1."),
 
         checkboxInput("dispDetails", "Display Detailed Results", value = FALSE, width = NULL),
+        checkboxInput("calcElast", "Calibrate Market Elasticity", value = FALSE, width = NULL),
+        conditionalPanel(
+          condition = "input.calcElast == false",
+          numericInput("enterElast", "Enter Market Elasticity:", value=-1,min=-Inf,max=0,step=.1#, width='75%'
+                       )
+        ),
         #checkboxInput("incEff", "Include Proportional Cost Changes (negative values imply cost reductions)", value = FALSE, width = NULL),
         
         radioButtons("supply", "Supply Specification:",
@@ -19,26 +25,42 @@ shinyUI(fluidPage(
                                 "Cournot"
                                 )),
         conditionalPanel(
-          condition = "input.supply == 'Bertrand'",
-          selectInput("demand_bert", "Demand Specification:",
-                      choices = c("logit", "ces", 
+          condition = "input.supply == 'Bertrand' && input.calcElast == true",
+          selectInput("demand_bert_alm", "Demand Specification:",
+                      choices = c("logit (unknown elasticity)", "ces (unknown elasticity)", 
                                   #"linear",
                                   "aids"))
         ),
         conditionalPanel(
-          condition = "input.supply == 'Cournot'",
+          condition = "input.supply == 'Bertrand'  && input.calcElast == false",
+          selectInput("demand_bert", "Demand Specification:",
+                      choices = c("logit", "ces", 
+                                  #"linear",
+                                  "pcaids"))
+        ),
+        conditionalPanel(
+          condition = "input.supply == 'Cournot'  && input.calcElast == true",
+          selectInput("demand_cournot_alm", "Demand Specification:",
+                      choices = c("linear (unknown elasticity)","log (unknown elasticity)"))
+        ),
+        conditionalPanel(
+          condition = "input.supply == 'Cournot'  && input.calcElast == false",
           selectInput("demand_cournot", "Demand Specification:",
                       choices = c("linear","log"))
         ),
-       
         conditionalPanel(
-          condition = "input.supply == '2nd Score Auction'",
+          condition = "input.supply == '2nd Score Auction' && input.calcElast == false",
           selectInput("demand_2nd", "Demand Specification:",
-                      choices = c("Logit"))
+                      choices = c("logit"))
+        ),
+        conditionalPanel(
+          condition = "input.supply == '2nd Score Auction' && input.calcElast == true",
+          selectInput("demand_2nd_alm", "Demand Specification:",
+                      choices = c("logit (unknown elasticity)"))
         ),
         conditionalPanel(
           condition = "input.supply == 'Cournot'",
-          helpText("Note: only the first inputted price and product name is used for Cournot.")
+          helpText("Note: only the first non-missing inputted price and product name is used for Cournot.")
           ),
         conditionalPanel(
           condition = "input.supply != '2nd Score Auction'",
@@ -47,6 +69,10 @@ shinyUI(fluidPage(
         conditionalPanel(
           condition = "input.supply == '2nd Score Auction'",
           helpText("Note: margins must be \u00A4/unit")
+        ),
+        conditionalPanel(
+          condition = "input.demand_bert == 'pcaids'",
+          helpText("Note: Only first non-missing inputted margin is used for pcaids. This margin should belong to a single product firm.")
         )
       ),
       mainPanel(
@@ -61,9 +87,13 @@ shinyUI(fluidPage(
           condition = "input.dispDetails == true",
           br(), br(),
           h2("Details"),
-          tableOutput("results_detailed") #,
-          #br(),br(),
-          #textOutput("mktElast")
+          tableOutput("results_detailed") ,
+          br(),br(),
+          radioButtons("pre_elast", "Product Elasticities:",
+                       choices = c("Pre-Merger",
+                                   "Post-Merger"
+                       ), inline = TRUE),
+          tableOutput("results_elast")
         )
       )
       
