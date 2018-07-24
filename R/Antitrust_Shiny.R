259926b1b716fa87ef88c94e30cbc8e5ac5bf7e2
Charles Taragin <Charles.Taragin@usdoj.gov>
2018-07-24 14:17:23 -0400

    restored deleted file

---
+++ R/Antitrust_Shiny.R
@@ -0,0 +1,23 @@
+#' @import shiny
+#' @title A Shiny Interface to the Antitrust Package
+#' @name antitrust_shiny
+#' @description A Shiny Interface to the Antitrust Package
+#' @details \code{antitrust_shiny} launches a shiny interface for
+#' the antitrust package. The shiny interface provides users with
+#' the ability to calibrate and simulate horizontal mergers using
+#' many of the supply and demand models included in the antitrust package.
+#' @author Charles Taragin \email{charles.taragin@usdoj.gov}
+#' @examples ## launch shiny interface to antitrust package
+#' \dontrun{
+#'   antitrust_shiny()
+#' }
+#' @include SimFunctions.R
+NULL
+
+## Function to launch shiny app.
+#'@rdname antitrust_shiny
+#'@export
+antitrust_shiny <- function() {
+  requireNamespace("rhandsontable")
+  shiny::runApp(system.file('antitrust_shiny', package='antitrust'))
+}
