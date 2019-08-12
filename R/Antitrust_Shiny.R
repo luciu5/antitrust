902b80fc6e4185cd2ad2b88bc9356c9cb869eab3
Wolak <pwolak@ftc.gov>
2019-08-12 17:14:25 -0400

    Added deprecation tags to shiny related files.

--- R/Antitrust_Shiny.R
+++ R/Antitrust_Shiny.R
@@ -1,23 +1,24 @@
-#' @import shiny
+
 #' @title A Shiny Interface to the Antitrust Package
-#' @name antitrust_shiny
 #' @description A Shiny Interface to the Antitrust Package
 #' @details \code{antitrust_shiny} launches a shiny interface for
 #' the antitrust package. The shiny interface provides users with
 #' the ability to calibrate and simulate horizontal mergers using
 #' many of the supply and demand models included in the antitrust package.
-#' @author Charles Taragin \email{ctaragin@ftc.gov}
-#' @examples ## launch shiny interface to antitrust package
-#' \dontrun{
-#'   antitrust_shiny()
-#' }
-#' @include SimFunctions.R
+#' @name antitrust_shiny-deprecated
+#' @seealso \code{\link{antitrust-deprecated}}
+#' @keywords internal
 NULL
 
-## Function to launch shiny app.
-#'@rdname antitrust_shiny
-#'@export
+#' @rdname antitrust-deprecated
+#' @section \code{antitrust_shiny}:
+#' For \code{antitrust_shiny}, use \code{\link[competitiontoolbox]{ct_shiny}}.
+#'
+#'
+#' @export
 antitrust_shiny <- function() {
-  requireNamespace("rhandsontable")
-  shiny::runApp(system.file('antitrust_shiny', package='antitrust'))
+  .Deprecated("ct_shiny' in the 'competitiontoolbox' package")
+  # requireNamespace("rhandsontable")
+  # shiny::runApp(system.file('antitrust_shiny', package='antitrust'))
 }
+
